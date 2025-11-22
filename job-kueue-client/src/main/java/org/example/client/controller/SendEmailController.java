51a32702077d21eafd1f68205bc9bacb51babc1f
d.s.samusev <d.s.samusev@tbank.ru>
2025-11-22 22:58:26 +0300

    Add SendEmailRequest validation

--- job-kueue-client/src/main/java/org/example/client/controller/SendEmailController.java
+++ job-kueue-client/src/main/java/org/example/client/controller/SendEmailController.java
@@ -1,6 +1,9 @@
 package org.example.client.controller;
 
+import jakarta.validation.Valid;
 import org.example.common.dto.SendEmailRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,8 +14,10 @@ public class SendEmailController {
     public static final String SEND_EMAIL = "/task/send_email";
 
     @PostMapping(SEND_EMAIL)
-    public void sendEmail(@RequestBody SendEmailRequest request) {
-        // заносим информацию о таске в базу
+    public ResponseEntity<Void> sendEmail(@RequestBody @Valid SendEmailRequest sendEmailRequest) {
+        // валидируем запрос
+        // заносим информацию о новой таске в базу
         // публикуем таску в кафка-топик
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
--- job-kueue-common/src/main/java/org/example/common/dto/SendEmailRequest.java
+++ job-kueue-common/src/main/java/org/example/common/dto/SendEmailRequest.java
@@ -1,13 +1,26 @@
 package org.example.common.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class SendEmailRequest {
 
-    private String recipient;
+    @NotBlank(message = "Поле recipient_email не может быть пустым")
+    @Email(message = "Поле recipient_email должно быть email-адресом")
+    @JsonProperty("recipient_email")
+    private String recipientEmail;
 
+    @NotBlank(message = "Поле subject не может быть пустым")
+    @Size(max = 255, message = "Тема письма не должна превышать 255 символов")
+    @JsonProperty("subject")
     private String subject;
 
+    @NotBlank(message = "Поле body не может быть пустым")
+    @Size(max = 10000, message = "Тело письма не должно превышать 10000 символов")
+    @JsonProperty("body")
     private String body;
 }
